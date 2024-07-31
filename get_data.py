44fce2b6d1a243dcd1faf9518c1463127adda391
Chris <cqolee@ucdavis.edu>
2024-07-30 17:00:42 -0700

    Merge branch 'main' of https://github.com/cjlee113/NBA-Predictor

--- get_data.py
+++ get_data.py
@@ -1,3 +1,49 @@
 import os
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeout
+import time 
+
+Seasons=list(range(2016,2024))
+
+#Create directories to store standings and scores information
+DATA_DIR = "data"
+STANDINGS_DIR = os.path.join(DATA_DIR, "standings")
+SCORES_DIR = os.path.join(DATA_DIR, "scores")
+
+#Function to read the html chart
+#Use async function to use playwright
+#Use await to not get timed out by BasketballReference
+async def get_html(url, selector, sleep=5, retries=3):
+    html = None
+    for i in range(1, retries+1):
+        time.sleep(sleep * i)
+        try:
+            async with async_playwright() as p:
+                browser = await p.chromium.launch()
+                page = await browser.new_page()
+                await page.goto(url)
+                print (await page.title())
+                html = await page.inner_html(selector)
+        except PlaywrightTimeout:
+            print(f"Timeout error on {url}")
+            continue
+        else:
+            break
+    return html
+# Scrapes the months from BasketballReference
+async def scrape_season(season):
+    url = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
+    html = await get_html(url, "#content .filter")
+    soup = BeautifulSoup(html)
+    links = soup.find_all("a")
+    href = [l["href"] for l in links]
+    standing_pages = [f"https://www.basketball-reference.com{l['href']}"for l in links]
+    for url in standing_pages:
+        save_path = os.path.join(STANDINGS_DIR, url.split("/")[-1])
+        if os.path.exists(save_path):
+            continue
+
+        html = await get_html(url, "all_schedule")
+        with open(save_path)
+
+
